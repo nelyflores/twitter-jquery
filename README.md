50cc4c4ada2ee0f7cc86877a42374d35a950f839
nelyflores <nely.afloresr@gmail.com>
2017-06-04 21:58:19 -0500

    agregando informacion al README

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ To Do App en jQ
 2. Agregar un evento de click al botón o un evento de submit al formulario.
 3. En el evento, agarrar el texto del textarea.
 4. Agregar el texto al HTML.
+**VERSIÓN 0.0.2**
+1. No ingresar texto vacío (deshabilitar el botón de enviar).
+2. Contar la cantidad de caracteres de forma regresiva.
+**VERSIÓN 0.0.3**
+1. Si pasa los 140 caracteres, deshabilitar el botón.
+2. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+3. Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+4. si pasa los 140 caracteres, mostrar el contador en negativo.
+
+**VERSIÓN 0.0.4**
+1. Al presionar enter ("/n") que crezca el textarea de acuerdo al tamaño del texto.
--- js/app.js
+++ js/app.js
@@ -1,48 +1,34 @@
 (function () {
-var contador = 0;
-
+	var contador = 0;
 	var cargarPagina = function () {
 		// Envío de To - Do
 		$("#to-do-form").submit(agregarToDo);
 		$("#message").keyup(validarContenido);
-
 	};
-
 	var agregarToDo = function (e) {
 		e.preventDefault();
-
 		var $contenedor = $("#posts");
 		var $mensajeContenedor = $("#message");
 		var $botonAgregar = $("#add-button");
 		var mensaje = $mensajeContenedor.val();
-
 		// Creamos elementos
 		var $postContenedor = $("<article />", { "class": "testimonial-section" });
 		//var $postCheck = $("<input type='checkbox' />");
 		var $postTexto = $("<label />");
-
 		var identificador = "marcador-" + contador;
-
 		//$postCheck.id = identificador;
-
 		$postTexto.attr("for", identificador);
 		$postTexto.text(mensaje);
-
-
 		// Agregarlos al DOM
 		//$postContenedor.append($postCheck);
 		$postContenedor.append($postTexto);
-
 		// Agregarlo a un elemento existente para visualizarlo
 		// contenedor.appendChild(postContenedor);
 		$contenedor.prepend($postContenedor);
-
 		// Borrar contenido de textarea
 		$mensajeContenedor.val("");
 		$botonAgregar.attr("disabled", true);
-
 		// bind, apply, call
-
 		contador++;
 	};
 	var validarContenido = function () {
@@ -54,48 +40,40 @@ var contador = 0;
 			$addButton.attr("disabled", true);
 		}
 	};
-
-
 	// Cuando carga la página
 	$(document).ready(cargarPagina);
-	})();
-	$(function(){
-		var $addButton = $("#add-button");
-		var longitud_max=140;
-		var carcateres_actuales=$("#message").val.length;
-		$(contador).html(longitud_max + " caracteres restantes");
-		$("#message").keyup(function(){
-			var caracteres_nuevos=longitud_max - $("#message").val().length;
-			$("#contador").html(caracteres_nuevos + "caracteres restantes");
+
+$(function(){
+	var $addButton = $("#add-button");
+	var longitud_max=140;
+	var carcateres_actuales=$("#message").val.length;
+	$(contador).html(longitud_max + " caracteres restantes");
+	$("#message").keyup(function(){
+		var caracteres_nuevos=longitud_max - $("#message").val().length;
+		$("#contador").html(caracteres_nuevos + "caracteres restantes");
 		if (caracteres_nuevos <=20 && caracteres_nuevos >10){
 			$("#contador").css("color","#DBA901");
 		}else if(caracteres_nuevos <=10){
 			$("#contador").css("color","#FF0000");
 
 		}else {
-				$("#contador").css("color","#000000");
+			$("#contador").css("color","#000000");
 		}
 		if(caracteres_nuevos <= 0){
 			$addButton.attr("disabled", true);
-
 		}
-
-
-		})
-	});
-
-	$("#message").on("keypress",function(e) {
-    var key = e.keyCode;
-		console.log(key);
-  var textarea = document.getElementById("message");
-
-    if (key == 13) {
-        textarea.rows += 1;
-
-    }
-		if (key == 8){
-	   	 textarea.rows -= 1;
-
-	   }
-
+	})
 });
+
+$("#message").on("keypress",function(e) {
+	var key = e.keyCode;
+	console.log(key);
+	var textarea = document.getElementById("message");
+	if (key == 13) {
+		textarea.rows += 1;
+	}
+	if (key == 8){
+		textarea.rows -= 1;
+	}
+});
+})();
